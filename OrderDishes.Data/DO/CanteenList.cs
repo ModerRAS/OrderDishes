08325e2700f20f3045fef24d2fdb9a09921bcb92
ModerRAS <adqewsx@gmail.com>
2023-10-13 17:33:46 +0800

    添加食堂和用户列表

---
+++ OrderDishes.Data/DO/CanteenList.cs
@@ -0,0 +1,11 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace OrderDishes.Data.DO {
+    public class CanteenList {
+        public required List<string> Canteens { get; set; }
+    }
+}
---
+++ OrderDishes.Data/DO/UserList.cs
@@ -0,0 +1,11 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace OrderDishes.Data.DO {
+    public class UserList {
+        public required List<string> Users { get; set; }
+    }
+}
