0e556d44481e0ffeb3deafb4ae96c338a3d69f3f
ModerRAS <adqewsx@gmail.com>
2023-10-13 17:35:41 +0800

    修改菜单和订单的结构，修改补充数据库设计

--- OrderDishes.Data/DO/Menu.cs
+++ OrderDishes.Data/DO/Menu.cs
@@ -4,5 +4,6 @@
         public DateTime Date { get; set; }
         public MenuTime Time { get; set; }
         public List<Guid> Foods {  get; set; }
+        public bool AlreadyStarted { get; set; }
     }
 }
--- OrderDishes.Data/DO/Order.cs
+++ OrderDishes.Data/DO/Order.cs
@@ -2,8 +2,12 @@
     public class Order {
         public Guid Id { get; set; }
         public DateTime CreatedDate { get; set; }
-        public Guid Canteen {  get; set; }
+        public string Canteen { get; set; }
+        public string User { get; set; }
         public MenuTime MenuTime { get; set; }
-        public List<Guid> Dishes { get; set; }
+        /// <summary>
+        /// 用`List<Food>`的原因是保留当时的订单相关的食物的原始数据
+        /// </summary>
+        public List<Food> Foods { get; set; }
     }
 }
