3a5a61093e6b160ec251adee401949b6548782d0
ModerRAS <adqewsx@gmail.com>
2024-02-11 16:21:04 +0800

    邀请码存储数据库设计

---
+++ OrderDishes.Data/DO/InvitationCode.cs
@@ -0,0 +1,12 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace OrderDishes.Data.DO {
+    public class InvitationCode {
+        public string UserInvitationCode { get; set; }
+        public string CanteenInvitationCode { get; set; }
+    }
+}
