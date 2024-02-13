fb4b99411bfc7638c416d4d59b67a163c36c9b73
ModerRAS <adqewsx@gmail.com>
2024-02-13 15:21:59 +0800

    食物编辑及其需要的文件格式转换功能

---
+++ OrderDishes.Common/Utility/PhotoTool.cs
@@ -0,0 +1,20 @@
+﻿using SkiaSharp;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace OrderDishes.Common.Utility {
+    public class PhotoTool {
+        public async Task ResizePhotoToJpeg(Stream stream) {
+            var img = SKBitmap.Decode(stream);
+            var width = img.Width;
+            var height = img.Height / (img.Width / 1280);
+            img.Resize(new SKImageInfo(1280, height), SKFilterQuality.None);
+            var img_data = img.Encode(SKEncodedImageFormat.Jpeg, 75);
+            //tg_img.Save(stream, ImageFormat.Jpeg);
+            var img_arr = img_data.ToArray();
+        }
+    }
+}
